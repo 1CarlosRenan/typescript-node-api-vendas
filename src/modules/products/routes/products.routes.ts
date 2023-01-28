1997c511639c7b9ba1e1ccf495845a043fb1191e
1CarlosRenan <carlosrenan96@gmail.com>
2023-01-27 21:52:00 -0300

    fix: routes and format code

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -7,7 +7,7 @@ const productsController = new ProductsController();
 productsRouter.get('/', productsController.index);
 productsRouter.get('/:id', productsController.show);
 productsRouter.post('/', productsController.create);
-productsRouter.put(':id', productsController.update);
+productsRouter.put('/:id', productsController.update);
 productsRouter.delete('/:id', productsController.delete);
 
 export default productsRouter;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,5 @@
-import productsRouter from '@modules/products/routes/products.routes';
 import { Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
 
 const routes = Router();
 
