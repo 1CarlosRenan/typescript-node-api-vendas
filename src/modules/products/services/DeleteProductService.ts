84944b560e69517935aee0c4ad5cf376f5061251
1carlosrenan <carlosrenan96@gmail.com>
2023-06-07 16:30:46 -0300

    refactor: redis

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,4 +1,4 @@
-import RedisCache from '@shared/cache/RedisCache';
+import redisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import ProductRepository from '../typeorm/repositories/ProductsRepository';
@@ -18,8 +18,6 @@ class DeleteProductService {
     }
 
     await productsRepository.remove(product);
-
-    const redisCache = new RedisCache();
 
     await redisCache.invalidate('api-vendas-PRODUCT_LIST');
   }
