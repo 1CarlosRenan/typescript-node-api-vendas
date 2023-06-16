337333a5edac7c3852a15833aa3aff729ca7b593
1carlosrenan <carlosrenan96@gmail.com>
2023-06-16 20:32:34 -0300

    refactor: update import customers

--- src/modules/customers/services/UpdateCustomerService.ts
+++ src/modules/customers/services/UpdateCustomerService.ts
@@ -1,7 +1,7 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
-import CustomersRepository from '../typeorm/repositories/CustomersRepository';
-import Customer from '../typeorm/entities/Customer';
+import CustomersRepository from '../infra/typeorm/repositories/CustomersRepository';
+import Customer from '../infra/typeorm/entities/Customer';
 
 interface IRequest {
   id: string;
