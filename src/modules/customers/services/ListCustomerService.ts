81f34eb754e39ed6407a4317bcf26c7787b6124b
1carlosrenan <carlosrenan96@gmail.com>
2023-07-08 21:30:38 -0300

    feat: pagination in customers

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,19 +1,32 @@
 import { inject, injectable } from 'tsyringe';
 import { ICustomersRepository } from '../domain/repositories/ICustomersRepository';
-import Customer from '../infra/typeorm/entities/Customer';
+import { ICustomerPaginate } from '../domain/models/ICustomerPaginate';
+
+interface SearchParams {
+  page: number;
+  limit: number;
+}
 
 @injectable()
 class ListCustomerService {
   constructor(
     @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
-  ) { }
+  ) {}
 
-  public async execute(): Promise<Customer[] | undefined> {
-    const customers = await this.customersRepository.findAll();
+  public async execute({
+    page,
+    limit,
+  }: SearchParams): Promise<ICustomerPaginate> {
+    const take = limit;
+    const skip = (Number(page) - 1) * take;
+    const customers = await this.customersRepository.findAll({
+      page,
+      skip,
+      take,
+    });
 
     return customers;
   }
 }
-
 export default ListCustomerService;
