4f46f3e1433756a87cad324e6cdd942ed76ea272
1carlosrenan <carlosrenan96@gmail.com>
2023-07-09 21:42:24 -0300

    update files

--- src/config/mail/HandlebarsMailTemplate.ts
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -10,7 +10,7 @@ interface IParseMailTemplate {
   variables: ITemplateVariable;
 }
 
-export default class HandlebarsMailTemplate {
+export default class handlebarsMailTemplate {
   public async parse({ file, variables }: IParseMailTemplate): Promise<string> {
     const templateFileContent = await fs.promises.readFile(file, {
       encoding: 'utf-8',
